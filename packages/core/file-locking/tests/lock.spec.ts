0d3560e5286deb14264eec597eb9e1094fd9fff7
Juan Picado <juanpicado19@gmail.com>
2022-02-12 17:15:18 +0100

    file-locking to esm

--- packages/core/file-locking/tests/lock.spec.ts
+++ packages/core/file-locking/tests/lock.spec.ts
@@ -1,7 +1,12 @@
 import fs from 'fs';
 import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 import { lockFile, readFile, unlockFile } from '../src/index';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 interface Error {
   message: string;
